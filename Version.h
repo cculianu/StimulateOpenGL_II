4dbab5fd584e6111e34aae6f1e8182db1d398294
Calin A. Culianu <calin.culianu@gmail.com>
2009-05-14 22:51:44 -0400

    Bumped version

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20080709
-#define VERSION_STR "StimulateOpenGL II v.20080709"
+#define VERSION 0x20090515
+#define VERSION_STR "StimulateOpenGL II v.20090515"
 
 #endif
