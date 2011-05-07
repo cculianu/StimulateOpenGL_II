6de2c39d3cd2f9005beace906ebad02c0c9474ca
Calin Culianu <Calin.Culianu@gmail.com>
2011-05-08 01:32:36 +0300

    Version bump

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20110504
-#define VERSION_STR "StimulateOpenGL II v.20110504"
+#define VERSION 0x20110508
+#define VERSION_STR "StimulateOpenGL II v.20110508"
 
 #endif
