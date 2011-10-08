00065b7bc2cc59bdabc63faaf98fcf3d6f5cbf90
Calin Culianu <Calin.Culianu@gmail.com>
2011-10-07 23:04:58 -0400

    Bumped app version

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20111006
-#define VERSION_STR "StimulateOpenGL II v.20111006"
+#define VERSION 0x20111007
+#define VERSION_STR "StimulateOpenGL II v.20111007"
 
 #endif
