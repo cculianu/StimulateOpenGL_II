95d716bffa54d21f37b0e7df1b87ff1346825843
Calin Culianu <Calin.Culianu@gmail.com>
2012-09-01 04:36:46 +0300

    convert avi to fmv added

--- Version.h
+++ Version.h
@@ -1,7 +1,7 @@
 #ifndef Version_H
 #define Version_H
 
-#define VERSION 0x20120831
-#define VERSION_STR "StimulateOpenGL II v.20120831"
+#define VERSION 0x20120901
+#define VERSION_STR "StimulateOpenGL II v.20120901"
 
 #endif
